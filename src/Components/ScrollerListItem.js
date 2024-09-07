e21380e7117c62057b08343b6306e5898948991b
kirkH <kirk@passivelogic.com>
2024-09-07 16:04:09 -0600

    fix: correct scroller

--- src/Components/ScrollerListItem.js
+++ src/Components/ScrollerListItem.js
@@ -1,22 +1,13 @@
 import React from "react";
 
 const ScrollerListItem = (props) => {
-    console.log(props)
   return (
-    <div className="flex-center">
-      <ul className="item-showcase">
-        <img
-          src={props.imgSrc}
-          alt="imgAlt"
-          className="image-kirk"
-        />
-        <div className="bio-wrapper">
-                  <div className="bio">
-                      {props.bio}
-                  </div>
-        </div>
-      </ul>
-    </div>
+    <ul className="item-showcase">
+      <img src={props.imgSrc} alt="imgAlt" className="image-kirk" />
+      <div className="bio-wrapper">
+        <div className="bio">{props.bio}</div>
+      </div>
+    </ul>
   );
 };
 
