81c20ec7ee2d60fdf80ff40fa1d2fc761ff80587
kirkH <kirk@passivelogic.com>
2024-09-11 20:59:15 -0600

    fix: case sensitive image names

--- src/Components/Scroller.js
+++ src/Components/Scroller.js
@@ -9,7 +9,7 @@ const Scroller = () => {
         bio="Hello there! 👋 My name is Kirk Hietpas and I am a Software Engineer currently living in Salt Lake City, UT. I currently work on the Cloud team at PassiveLogic building back-end cloud services using Server Side Swift, Vapor, PostgresQL and GraphQL. I have a deep passion for learning and thoroughly enjoy working as a Software Engineer."
       />
       <ScrollerListItem
-        imgSrc={ "running.jpg"}
+        imgSrc={ "running-mtns.jpg"}
         bio="In 2021 I graduated with a Master of Software Development from the University of Utah. In 2015 I graduated with a Bachelor of Biology with a minor in Chemistry from the University of Minnesota-Duluth."
       />
       <ScrollerListItem
