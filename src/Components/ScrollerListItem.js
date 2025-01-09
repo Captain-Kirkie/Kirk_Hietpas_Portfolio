9f9353c62b34825642005c19b16845cd5a101982
KirkHietpas <hietpaskirk@gmail.com>
2025-01-08 20:13:30 -0700

    lazy loading images

--- src/Components/ScrollerListItem.js
+++ src/Components/ScrollerListItem.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 const ScrollerListItem = (props) => {
-  return (
-    <ul className="item-showcase">
-      <img src={props.imgSrc} alt="imgAlt" className="image-kirk" />
-      <div className="bio-wrapper">
-        <div className="bio">{props.bio}</div>
-      </div>
-    </ul>
-  );
+    return (
+        <ul className="item-showcase">
+            <img
+                src={props.imgSrc}
+                alt="imgAlt"
+                className="image-kirk"
+                loading="lazy"
+            />
+            <div className="bio-wrapper">
+                <div className="bio">{props.bio}</div>
+            </div>
+        </ul>
+    );
 };
 
 export default ScrollerListItem;
